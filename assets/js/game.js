19cdd23afce7a026cb319158cba7838ec3df169b
oak45985 <oak45985@gmail.com>
2022-06-13 23:15:39 -0500

    webstorage api

--- assets/js/game.js
+++ assets/js/game.js
@@ -147,12 +147,28 @@ var startGame = function() {
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
 
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you survived the game! You have a score of " + playerInfo.money +".");
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
     }
+
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+
     else {
-        window.alert("You've lost your robot in battle :(.");
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
+
+    // if (playerInfo.health > 0) {
+    //     window.alert("Great job, you survived the game! You have a score of " + playerInfo.money +".");
+    // }
+    // else {
+    //     window.alert("You've lost your robot in battle :(.");
+    // }
 
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
